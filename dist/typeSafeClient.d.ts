3af7c65628148a8a62bbc4c4be9aebfeeda11a87
hemanth <hemanth.hm@gmail.com>
2026-09-18 17:37:13 -0700

    chore: untrack dist folder

--- dist/typeSafeClient.d.ts
+++
@@ -1,8 +0,0 @@
-import { choice, score, noul, type Questions, type SystemOneRequest, type SystemOneResult } from "@typesafe-ai/sdk";
-export { choice, score, noul };
-export interface ITypeSafeClient {
-    systemOne<const Q extends Questions>(request: SystemOneRequest<Q>): Promise<SystemOneResult<Q>>;
-    readonly isLive: boolean;
-}
-export declare function getTypeSafeClient(): ITypeSafeClient;
-//# sourceMappingURL=typeSafeClient.d.ts.map
--- dist/types.js
+++
@@ -1,5 +0,0 @@
-/**
- * Domain types for TypeSafe-powered Chess Moves and Games.
- */
-export {};
-//# sourceMappingURL=types.js.map
